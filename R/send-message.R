301f274ce68d0b85e0074e3aec89dbac4907eaf1
Daniel Amaral <silva.daniel86@Gmail.com>
2022-03-04 22:01:36 -0300

    :rocket: first version of bot

---
+++ R/send-message.R
@@ -0,0 +1,28 @@
+# Initialize bot
+bot <- telegram.bot::Bot(token = telegram.bot::bot_token("verbs"))
+
+# Get updates
+updates <- bot$getUpdates()
+
+# Retrieve chat ids
+chat_ids <- sapply(updates, function(msg) msg$from_chat_id())
+
+# Create daily verbs message
+seed <- as.numeric(Sys.time())
+set.seed(seed)
+
+verbs <- read.csv("data/verbs.csv", fileEncoding = "utf-8")
+daily_verbs <- verbs[sample(x = 1:nrow(verbs), size = 2),]
+
+# Send message
+purrr::walk(unique(chat_ids), function(chat_id) {
+  msg_head <- paste0("_Today Verbs_ (", format(Sys.time(), '%d/%m/%y'), ")")
+  msg_body <- purrr::map_chr(1:nrow(daily_verbs), function(rw) {
+    paste0("Base form: *", daily_verbs[rw,]$infinitivo, "*<br>",
+           "Past form: *", daily_verbs[rw,]$passado_simples, "*<br>",
+           "Past participle: *", daily_verbs[rw,]$participio_passado, "*<br>",
+           "Portuguese translation: *", daily_verbs[rw,]$significado, "*")
+  })
+  msg <- paste0(msg_head, paste0(msg_body, collapse = "<br>---<br>"), collapse = "<br>")
+  bot$sendMessage(chat_id, text = msg, parse_mode = "Markdown")
+})
